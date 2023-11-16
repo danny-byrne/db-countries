30f7745e6a5b8d3561228aec69094cb8375a1027
Danny Byrne <danny.byrne.dev@gmail.com>
2023-11-15 16:00:27 -0800

    chart is rendering with dummy data

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -6,12 +6,15 @@ const FLAG_HEIGHT = (3 / 5) * FLAG_WIDTH;
 
 import { formatNumberWithCommas } from "../helpers";
 
-const CountryCard = ({ country }) => {
-  const { name, capital, population, area, flag, continent } = country;
+const CountryCard = ({ country, onClick }) => {
+  const { name, capital, population, area, flag } = country;
   const formattedPopulation = formatNumberWithCommas(population);
   const formattedArea = formatNumberWithCommas(area);
   return (
-    <div className="w-96 md:w-1/2 h-24 bg-gray-100 text-black border-zinc-700 rounded pl-3 pt-2 shadow-lg shadow-indigo-500/40 text-gray-800">
+    <div
+      className="w-96 md:w-1/2 h-24 bg-gray-100  border-zinc-700 rounded pl-3 pt-2 shadow-lg shadow-indigo-500/40 hover:shadow-indigo-700/40 hover:cursor-pointer text-gray-800"
+      onClick={onClick}
+    >
       <div className="flex flex-row">
         <div className="flex flex-col w-1/2 gap-1 text-xs">
           {name}
