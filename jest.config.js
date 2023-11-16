a6dc00406cafb6350a92ff689b77ab63020dd879
Danny Byrne <danny.byrne.dev@gmail.com>
2023-11-15 17:50:49 -0800

    troubleshooting jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 // jest.config.js
 module.exports = {
+  rootDir: __dirname,
+  // setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
   testEnvironment: "node",
   transform: {
     "^.+\\.js$": "babel-jest",
---
+++ src/components/Countrycard.test.js
@@ -0,0 +1,35 @@
+// components/CountryCard.test.js
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+// import "@testing-library/jest-dom/extend-expect";
+import CountryCard from "./CountryCard";
+
+const mockCountry = {
+  name: "Test Country",
+  capital: "Test Capital",
+  population: 1000000,
+  area: 5000,
+  flag: "test-flag-url",
+};
+
+test("renders CountryCard component correctly", () => {
+  render(<CountryCard country={mockCountry} onClick={() => {}} />);
+
+  expect(screen.getByText(/Test Country/)).toBeInTheDocument();
+  expect(screen.getByText(/Test Capital/)).toBeInTheDocument();
+  expect(screen.getByText(/Population: 1,000,000/)).toBeInTheDocument();
+  expect(screen.getByText(/Area: 5,000 km\/sq/)).toBeInTheDocument();
+  expect(screen.getByAltText("Country Flag")).toHaveAttribute(
+    "src",
+    "test-flag-url"
+  );
+});
+
+// test("calls onClick when card is clicked", () => {
+//   const mockOnClick = jest.fn(); // Create a mock function
+
+//   render(<CountryCard country={mockCountry} onClick={mockOnClick} />);
+
+//   fireEvent.click(screen.getByText(/Test Country/));
+//   expect(mockOnClick).toHaveBeenCalledWith();
+// });
